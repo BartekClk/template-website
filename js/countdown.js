12b91051c537d4dcd48eb0cc98fd10278ed4795e
Bartek <clkbartek@gmail.com>
2022-08-08 00:07:46 +0200

    First commit

---
+++ js/countdown.js
@@ -0,0 +1,16 @@
+var x = setInterval(function() {
+    countDownDate = new Date("oct 4 2021 12:00:00 GMT+0200").getTime();;
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
